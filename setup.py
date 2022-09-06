cc68e7807de5580f538fb3f84f5914362fbb4cbf
VARVARI Cosmin <cvarvari@jouve.com>
2022-09-06 10:28:24 +0200

    feat: Updated setup.py again

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ tests_require = [
 ]
 
 setup(name='debugging_cake_portal',
-      version='1.0',
+      version='0.0.1',
       description="Luminess debugging forum.",
       author="Python Intern Team Luminess",
       install_requires=install_requires,

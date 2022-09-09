c784600dc4545039f631fa881190203682b9d1c1
VARVARI Cosmin <cvarvari@jouve.com>
2022-09-09 10:36:10 +0200

    test: new version for setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(name='debugging_cake_portal',
       install_requires=install_requires,
       package_dir={"": "src"},
       #packages=find_packages("src", exclude=("tests",)),
-      packages=["debugging_cake_portal", "cake_user", "posts"],
+      packages=["debugging_cake_portal", "debugging_cake_portal.cake_user", "debugging_cake_portal.posts"],
       test_suite="tests",
       tests_require=tests_require,
       extras_require={

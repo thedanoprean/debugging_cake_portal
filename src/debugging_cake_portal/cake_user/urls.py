eb8177ef18f93ab7cb89cf6ebecf059eb9211526
RUSU Tudor <trusu@jouve.com>
2022-09-27 11:25:33 +0200

    fix: cake_user urls conflict

--- src/debugging_cake_portal/cake_user/urls.py
+++ src/debugging_cake_portal/cake_user/urls.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
-
 from posts.views import PostListView
 from .views.homepage_view import homepage
 from .views.login_view import LoginView
@@ -10,11 +9,8 @@ from .views.register_view import register
 app_name = "cake_user"
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', homepage, name='home'),
-=======
     path('', PostListView.as_view()),
->>>>>>> feature/search2
     path('login/', LoginView.as_view(template_name='cake_user/login.html'), name='login'),
     path('register/', register, name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
